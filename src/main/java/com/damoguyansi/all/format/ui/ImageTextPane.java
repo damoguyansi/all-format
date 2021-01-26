cc75b489ea833a41ec0f2a5d6c136baba17afaad
damoguyansi <damoguyansi@163.com>
2021-01-26 14:49:45 +0800

    优化二维码容器

--- src/main/java/com/damoguyansi/all/format/ui/ImageTextPane.java
+++ src/main/java/com/damoguyansi/all/format/ui/ImageTextPane.java
@@ -1,24 +1,30 @@
 package com.damoguyansi.all.format.ui;
 
-import com.sun.media.sound.SoftTuning;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
-public class ImageTextPane extends JTextPane {
+public class ImageTextPane extends JTextPane implements ActionListener {
+    private final Set<Integer> pressed = new HashSet<Integer>();
 
-    public ImageTextPane(){
-        System.out.println(".......");
+    public ImageTextPane() {
+        KeyStroke copy1 = KeyStroke.getKeyStroke(KeyEvent.VK_V, Event.CTRL_MASK, false);
+        KeyStroke copy2 = KeyStroke.getKeyStroke(KeyEvent.VK_V, Event.META_MASK, false);
+        this.registerKeyboardAction(this, "Paste", copy1, JComponent.WHEN_FOCUSED);
+        this.registerKeyboardAction(this, "Paste", copy2, JComponent.WHEN_FOCUSED);
     }
 
     @Override
     public void paste() {
-        System.out.println("=======");
         if (isEditable() && isEnabled()) {
             Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
             Transferable contents = cb.getContents(null);
@@ -36,6 +42,15 @@ public class ImageTextPane extends JTextPane {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getSource());
+        System.out.println(e.getActionCommand());
+        if (e.getActionCommand().compareTo("Paste") == 0) {
+            this.paste();
         }
     }
 }
